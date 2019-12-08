315a624119da8afd48e3f9d7c5f5d7274d0f74c7
jianghy <tzword@163.com>
2019-12-08 21:00:50 +0800

    lazy懒加载

--- src/main/java/com/tzword/config/MainConfig2.java
+++ src/main/java/com/tzword/config/MainConfig2.java
@@ -4,6 +4,7 @@ import com.tzword.bean.Person;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
@@ -20,9 +21,13 @@ public class MainConfig2 {
      * singleton:单实例(默认值),ioc容器启动会调用方法创建对象放到ioc容器中。以后每次获取的就是直接从容器(map.get())中拿
      * request:同一次请求创建一个实例
      * session:同一个session创建一个实例
+     *
+     * 懒加载：单实例bean：默认在容器启动的时候创建对象；
+     *         懒加载：容器启动不创建对象。第一次使用(获取)Bean创建对象，并初始化
      */
-    @Scope("prototype")
+//    @Scope("prototype")
     @Bean("person")
+    @Lazy
     public Person person(){
         System.out.println("给容器中添加Person....");
         return new Person("王五","25");
