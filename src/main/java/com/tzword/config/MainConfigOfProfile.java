f9a515a12497db8717fd2c6b993d17288b59224d
jianghy <haoyuan_jiang@ssic.cn>
2019-12-19 11:26:46 +0800

    自动装配-@Profile根据环境注册bean

--- src/main/java/com/tzword/config/MainConfigOfProfile.java
+++ src/main/java/com/tzword/config/MainConfigOfProfile.java
@@ -1,10 +1,12 @@
 package com.tzword.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import com.tzword.bean.Yellow;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.util.StringValueResolver;
 
@@ -16,7 +18,14 @@ import java.beans.PropertyVetoException;
  *      Spring为我们提供可以根据当前环境，动态的激活和切换一系列组件的功能
  *      开发环境、测试环境、生产环境
  *      数据源（/A）（/B）（/C）
+ *
+ * @Profile:指定组件在哪个环境下才能被注册到容器中，不指定，任何环境下都能注册这个组件
+ *
+ * 1）、加了环境标识的bean，只有这个环境被激活的时候才能注册到容器中.默认是default环境
+ * 2）、写在配置类上，只有是指定的环境的时候，整个配置类里面的所有配置才能开始生效
+ * 3）、没有标注环境标识的bean在任何环境下都是可以加载的
  */
+//@Profile("dev")
 @Configuration
 @PropertySource("classpath:/dbconfig.properties")
 public class MainConfigOfProfile implements EmbeddedValueResolverAware {
@@ -33,6 +42,13 @@ public class MainConfigOfProfile implements EmbeddedValueResolverAware {
     @Value("${db.user}")
     private String user;
 
+    //@Profile("dev")
+    @Bean
+    public Yellow yellow(){
+        return new Yellow();
+    }
+
+    @Profile("dev")
     @Bean("devDataSource")
     public DataSource dataSourceDev(@Value("${db.password}")String password) throws PropertyVetoException {
         ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
@@ -43,6 +59,7 @@ public class MainConfigOfProfile implements EmbeddedValueResolverAware {
         return comboPooledDataSource;
     }
 
+    @Profile("test")
     @Bean("testDataSource")
     public DataSource dataSourceTest(@Value("${db.password}")String password) throws PropertyVetoException {
         ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
@@ -53,6 +70,7 @@ public class MainConfigOfProfile implements EmbeddedValueResolverAware {
         return comboPooledDataSource;
     }
 
+    @Profile("prod")
     @Bean("prodDataSource")
     public DataSource dataSourceProd(@Value("${db.password}")String password) throws PropertyVetoException {
         ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
--- src/test/java/com/tzword/test/IocTestProfile.java
+++ src/test/java/com/tzword/test/IocTestProfile.java
@@ -1,5 +1,6 @@
 package com.tzword.test;
 
+import com.tzword.bean.Yellow;
 import com.tzword.config.MainConfigOfAutowired;
 import com.tzword.config.MainConfigOfProfile;
 import org.junit.Test;
@@ -8,14 +9,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import javax.sql.DataSource;
 
 public class IocTestProfile {
+
+    //1、使用命令行动态参数（运行的时候加上，在VM options:-Dspring.profiles.active=dev,那运行的就是dev环境）
+    //2、代码的方式激活某种环境
     @Test
     public void test01(){
-        //创建ioc容器
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfProfile.class);
+        //1.创建ioc容器
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        //2.设置激活的环境
+        applicationContext.getEnvironment().setActiveProfiles("prod");
+        //3.注册配置类
+        applicationContext.register(MainConfigOfProfile.class);
+        //4.启动刷新容器
+        applicationContext.refresh();
+
         String[] beanNamesForType = applicationContext.getBeanNamesForType(DataSource.class);
         for (String s : beanNamesForType) {
             System.out.println(s);
         }
+
+        Yellow bean = applicationContext.getBean(Yellow.class);
+        System.out.println(bean);
         //关闭容器
         applicationContext.close();
 
